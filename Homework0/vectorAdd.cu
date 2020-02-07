265826e5ebda959e2131f19e7e7d0abe9add2357
bhavin392 <thakar.bhavin38@gmail.com>
2020-02-06 19:01:29 -0500

    Initial Commit

--- Homework0/vectorAdd.cu
+++ Homework0/vectorAdd.cu
@@ -1,25 +1,55 @@
-#define N (1024*1024)
-// #define THREADS_PER_BLOCK 512
+#include<stdio.h>
+#include <sys/time.h>
+struct timeval stop, start,start1,stop1;
+#define N (2048*2048)
+#define THREADS_PER_BLOCK 1024
 
 
 __global__ void add(int *a, int *b, int *c, int n){
     int index =threadIdx.x + blockIdx.x * blockDim.x;
-    if(intex<n){
+    if(index<n){
         c[index]=a[index]+b[index];
     }
 }
+void random(int *a, int n ){
+   int i;
+   for (i = 0; i < n; ++i)
+    a[i] = rand()%100;
+    
+}
 
-int main(int argc, char * argv[]){
+int main(){
     int *a, *b, *c;
-    int *d_a, *d_b, *d_c;
     int size= N*sizeof(int);
+    a=(int*)malloc(size);
+    b=(int*)malloc(size);
+    c=(int*)malloc(size);
+
+    int *d_a, *d_b, *d_c;
 
     cudaMalloc((void ** )&d_a,size);
     cudaMalloc((void **)&d_b,size);
-    cudaMalloc((void **)&d_c,size);
+    cudaMalloc((void **)&d_c,size);  
 
-    a=(int *)malloc(size); random_ints(a,N);
-    b=(int *)malloc(size); random_ints(b,N);
-    c=(int *)malloc(size);
+    random(a,N);
+    random(b,N);
+
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    // Launch add() kernel on GPU
+    gettimeofday(&start, NULL);
+    add<<<(N + THREADS_PER_BLOCK-1) / THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a, d_b, d_c,N);
+    gettimeofday(&stop, NULL);
+    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    cudaDeviceSynchronize();
+    // Copy result back to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    // Cleanup
+    free(a); 
+    free(b); 
+    free(c);
+    cudaFree(d_a); 
+    cudaFree(d_b); 
+    cudaFree(d_c);
+    return 0;
 }
-
